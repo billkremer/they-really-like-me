c083cabcc281e992e15ae9ce603638fc63206562
Bill Kremer <bkrems12@gmail.com>
2017-01-19 13:51:39 -0600

    added ajax call and displayPeople function
    created the ajax call to retrieve data and display the people info.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,25 @@ $(function () {
 
 
 
+  $.ajax({
+    url: '/bios',
+    type: 'GET',
+    success: displayPeople
+  });
+
+
+
+
 });
+
+
+
+function displayPeople(teamMembers) {
+  console.log(teamMembers);  // teamMembers is a placeholder for the people object in app.js ( and data.json )
+
+  console.log("does this even work?");
+
+
+
+
+};
