1ba5694da6cf0c5d51740414e1e6c93a31cfc566
Bill Kremer <bkrems12@gmail.com>
2017-01-20 11:57:41 -0600

    refactored for dymaic likes POSTS

--- app.js
+++ app.js
@@ -21,27 +21,48 @@ app.get('/', function(req, res) {
 });
 
 app.get('/bios', function(req, res) {
+  // sends 'people' back to the request
   res.send(people);
 });
 
 app.get('/likes', function(req, res) {
+  // sends likesArray back to the request
   res.send(likesArray);
 });
 
 
 // hard coded posts
-app.post('/likes/PriscillaGyamfi', function(req, res) {
-  likesArray[2].likes++;
-  res.sendStatus(200);
-});
+// app.post('/likes/PriscillaGyamfi', function(req, res) {
+// //
+//   likesArray[2].likes++;
+//   res.sendStatus(200);
+// });
+//
+// app.post('/likes/ScottFox', function(req, res) {
+//   likesArray[0].likes++;
+//   res.sendStatus(200);
+// });
+//
+// app.post('/likes/BillKremer', function(req, res) {
+//   likesArray[1].likes++;
+//   res.sendStatus(200);
+// });
 
-app.post('/likes/ScottFox', function(req, res) {
-  likesArray[0].likes++;
-  res.sendStatus(200);
-});
+// req.body for these three was = { BillKremer, ""}
+// probably could refactor a bit more dynamically
+// req.body.name could be used with some work...
 
-app.post('/likes/BillKremer', function(req, res) {
-  likesArray[1].likes++;
+
+// dynamic way to do it
+app.post('/likes/:person', function (req, res) {
+//    req.params.person = name  ( name is concatinated)
+// console.log('req.params', req.params);
+
+  likesArray.forEach( function (temp) {
+    if (temp.name.split(" ").join("") ==  req.params.person) {
+      temp.likes++;
+    };
+  });
   res.sendStatus(200);
 });
 
