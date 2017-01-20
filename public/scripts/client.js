4d0fb0263d53ad5badb87c29f5f11f4bd98b3338
Bill Kremer <bkrems12@gmail.com>
2017-01-19 18:10:06 -0600

    created get and  post requests for likes
    created a get request to obtain the current likes
    created post requess to update the likes when a like button is pressed.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,12 +9,41 @@ $(function () {
     success: displayPeople
   });
 
+  $('#container').on('click', 'button', function () {
+      var holder = $(this).attr('id'); // holder is the holder for the ID string on the button that was clicked
+      holder = holder.substring(1); // strips off the # from the ID
+      updateLikes(holder);
+  });
+
+}); // doc ready closing
+
+function updateLikes (nameToGetMoreLikes) {
+  $.ajax({
+    url: '/likes',
+    type: 'GET',
+    success: getLikeCount
+  });
 
 
+console.log(nameToGetMoreLikes +'name');
+  $.ajax({
+    url: '/likes/' + nameToGetMoreLikes, // creates individual POST calls
+    type: 'POST',
+    success: updateCountonDom
+  });
+}
 
-});
 
+function updateCountonDom(){
+  console.log('insideupdatecountondom');
+}
 
+function getLikeCount(temp){
+  console.log(temp);
+  console.log('insidegetlikecount');
+    // console.log(nameToGetMoreLikes);
+  // console.log('insidegetlikecount');
+}
 
 function displayPeople(teamMembers) {
   console.log(teamMembers);  // teamMembers is a placeholder for the people object in app.js ( and data.json )
@@ -25,7 +54,7 @@ function displayPeople(teamMembers) {
 
     $personDiv.append('<p>' + individual.bio + '</p>');
     $personDiv.append('<img src="' + individual.imageurl + '"/> ');
-    $personDiv.append('<button>Like</button>');
+    $personDiv.append('<button id=#' + individual.name + '>Like</button>');
     $('#container').append($personDiv);
 
   });
