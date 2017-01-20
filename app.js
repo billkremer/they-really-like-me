d344f8447eaf57bfcee83d1ab1a27b0597c0dbbb
Bill Kremer <bkrems12@gmail.com>
2017-01-19 18:09:23 -0600

    added individual like POST routes
    created a likes Array for all the likes

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var people = require('./routes/data.json'); // need to update
+var people = require('./routes/data.json');
+
+var likesArray = [{name: 'Scott Fox', likes: 1},
+                  {name: 'Bill Kremer', likes: 2},
+                  {name: 'Priscilla Gyamfi', likes: 3}];
+
+
+
 
 var app = express();
 
@@ -19,8 +26,28 @@ app.get('/bios', function(req, res) {
 });
 
 app.get('/likes', function(req, res) {
-
+  res.send(likesArray);
 });
+
+// app.post('/likes'), function(req, res) {
+//
+// }
+
+app.post('/likes/Priscilla', function(req, res) {
+  likesArray[2].likes++;
+  console.log(likesArray[2].likes+ 'plikes');
+});
+
+app.post('/likes/Scott', function(req, res) {
+  likesArray[0].likes++;
+  console.log(likesArray[0].likes+ 'slikes');
+});
+
+app.post('/likes/Bill', function(req, res) {
+  likesArray[1].likes++;
+  console.log(likesArray[1].likes + 'billlikes');
+});
+
 
 
 app.listen(3000);
