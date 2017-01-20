4e6d6e4c7d3b224ed6741065efec531f9ef12dd5
Bill Kremer <bkrems12@gmail.com>
2017-01-20 01:02:59 -0600

    completed functions per customer request
    finished writing functionns to place likes on to the DOM.  completed the necessary ajax requests.
    
    Also fixed issues with using names for the IDs and rettrieving the data.
    
    Added alt tags to the images

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,41 +1,45 @@
 $(function () {
-  console.log('doc ready');
 
+getPeople();
 
-
-  $.ajax({
-    url: '/bios',
-    type: 'GET',
-    success: displayPeople
-  });
 
   $('#container').on('click', 'button', function () {
-      var holder = $(this).attr('id'); // holder is the holder for the ID string on the button that was clicked
-      holder = holder.substring(1); // strips off the # from the ID
-      updateLikes(holder);
+      updateLikes($(this).attr('id'));
   });
 
 }); // doc ready closing
 
+
 function updateLikes (nameToGetMoreLikes) {
-  $.ajax({
-    url: '/likes',
-    type: 'GET',
-    success: getLikeCount
-  });
 
-
-console.log(nameToGetMoreLikes +'name');
   $.ajax({
     url: '/likes/' + nameToGetMoreLikes, // creates individual POST calls
     type: 'POST',
-    success: updateCountonDom
+    data: nameToGetMoreLikes,
+    success: getLikes
   });
 }
 
 
-function updateCountonDom(){
-  console.log('insideupdatecountondom');
+function getLikes(){
+// should update all on dom?
+  $.ajax({
+    url: '/likes/', // creates individual POST calls
+    type: 'GET',
+    // data: nameToGetMoreLikes,
+    success: updateCountonDom
+  });
+}
+
+function updateCountonDom (likesArray) {
+//   $('.likes').empty(); // would empty all likes, but not necessary because of .text() below
+
+  likesArray.forEach( function (individual) {
+    var idPlaceholder = individual.name.split(" ").join("")
+
+    $("#container").find("#"+idPlaceholder).next('span').text(individual.likes + " Likes!");
+  });
+
 }
 
 function getLikeCount(temp){
@@ -45,22 +49,32 @@ function getLikeCount(temp){
   // console.log('insidegetlikecount');
 }
 
+
+function getPeople() {
+  $.ajax({
+    url: '/bios',
+    type: 'GET',
+    success: displayPeople
+  });
+};
+
+
 function displayPeople(teamMembers) {
-  console.log(teamMembers);  // teamMembers is a placeholder for the people object in app.js ( and data.json )
+// teamMembers is a placeholder for the people object in app.js ( and data.json )
+  var idName = "";
 
   teamMembers.forEach( function (individual) {
+    idName = individual.name.split(" ").join("");
+    // creates an id name of their whole name without spaces.
+
+    // buildup of div text
     var $personDiv = $('<div><h2>' + individual.name + '</h2></div>');
 
-
     $personDiv.append('<p>' + individual.bio + '</p>');
-    $personDiv.append('<img src="' + individual.imageurl + '"/> ');
-    $personDiv.append('<button id=#' + individual.name + '>Like</button>');
+    $personDiv.append('<img src="' + individual.imageurl + '" alt="Photo of ' + individual.name + '"/> ');
+    $personDiv.append('<button id="' + idName + '">Like</button>');
+    $personDiv.append('<span class="likes">0 Likes!</span>');
+
     $('#container').append($personDiv);
-
   });
-
-
-
-
-
 };
